118d26a41e0223b44a87cc890d398462c04d6779
Stephen-O1 <onyangostephen654@gmail.com>
2025-10-02 20:43:41 +0200

    Added some other image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
       <div  className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero_background.png" />
             <h1><span className='text-gradient'>Movies Hub:</span> Chills and Flix</h1>
           </header>
           <p>Search</p>
